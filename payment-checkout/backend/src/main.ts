e37a726156ba0f9cfb40ac873f73c10852f073c7
Kellycaro1993 <kellycaro1993@gmail.com>
2026-08-15 12:20:32 -0500

    omitiendo console

--- payment-checkout/backend/src/main.ts
+++ payment-checkout/backend/src/main.ts
@@ -3,11 +3,6 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
 
-console.log('cwd:', process.cwd());
-console.log(
-  'integrity loaded:',
-  Boolean(process.env.WOMPI_INTEGRITY_SECRET),
-);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
