0ca78f67e094b2a8c5ee0a8dcb2819185a63ccd0
Kellycaro1993 <kellycaro1993@gmail.com>
2026-08-14 21:05:21 -0500

    agregando controlador

--- payment-checkout/backend/src/modules/products/controllers/products.controller.ts
+++ payment-checkout/backend/src/modules/products/controllers/products.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { ProductsService } from '../services/products.service';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(
+    private readonly productsService: ProductsService,
+  ) {}
+
+  @Get()
+  async getProducts() {
+    return this.productsService.getProducts();
+  }
+
+  @Get(':id')
+  async getProductById(
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.productsService.getProductById(id);
+  }
 }
--- payment-checkout/backend/src/modules/products/products.module.ts
+++ payment-checkout/backend/src/modules/products/products.module.ts
@@ -2,15 +2,16 @@ import { Module } from '@nestjs/common';
 import { ProductsRepository } from './repositories/products.repository';
 import { PrismaProductsRepository } from './repositories/prisma-products.repository';
 import { ProductsService } from './services/products.service';
-
+import { ProductsController } from './controllers/products.controller';
 @Module({
   providers: [
     {
       provide: ProductsRepository,
       useClass: PrismaProductsRepository,
     },
-    ProductsService,
+    ProductsService, 
   ],
   exports: [ProductsRepository],
+  controllers: [ProductsController]
 })
 export class ProductsModule {}
